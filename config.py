26e98aaadd25cfa1d8a098aea62003968bd6878e
Umaidahmad01 <ahmedumaid707@gmail.com>
2024-11-23 23:56:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,9 +31,8 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "5585016974"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "0")
-DB_NAME = os.environ.get("DATABASE_NAME", "0")
-
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://uffobitoxe:umaid2008@cluster0.vpebe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001930406310"))
 
